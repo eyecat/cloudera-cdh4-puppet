4ec92bbad92e82f0bd47ee80bd39fae912033173
John Anderson <sontek@gmail.com>
2013-03-29 17:54:23 -0700

    fix post-bootstrap key sync

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,9 +1,9 @@
 log_level                :info
 log_location             STDOUT
 node_name                'admin'
-client_key               '/home/yuhui/VagrantWork/cloudera-cdh4-puppet/.chef/admin.pem'
+client_key               'admin.pem'
 validation_client_name   'chef-validator'
-validation_key           '/home/yuhui/VagrantWork/cloudera-cdh4-puppet/.chef/chef-validator.pem'
+validation_key           'chef-validator.pem'
 chef_server_url          'https://33.33.66.100'
 cache_type               'BasicFile'
-cache_options( :path => '/home/yuhui/VagrantWork/cloudera-cdh4-puppet/.chef/checksums' )
+cache_options( :path => 'checksums' )
--- post-bootstrap.sh
+++ post-bootstrap.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+sudo rm -f /vagrant/.chef/*.pem
 sudo cp /etc/chef-server/*.pem /vagrant/.chef/ -f -n
